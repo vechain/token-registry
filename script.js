c347f3ff9a464583d2f0e7e283b280e03c14ad12
Asbert Ma <stayinalive@msn.cn>
2019-06-06 11:12:58 +0800

    order by create time

--- script.js
+++ script.js
@@ -2,17 +2,17 @@ const file = require('file-system')
 const fs = require('fs')
 const path = require('path')
 const hashName = require('hash-file')
+const { exec } = require('child_process')
 
-
-redFont = (str) => {
+redFont = str => {
   return `\x1b[31m${str}\x1b[0m`
 }
 
-greenFont = (str) => {
+greenFont = str => {
   return `\x1b[32m${str}\x1b[0m`
 }
 
-yellowFont = (str) => {
+yellowFont = str => {
   return `\x1b[33m${str}\x1b[0m`
 }
 
@@ -40,18 +40,26 @@ const clear = () => {
   console.timeEnd(greenFont('clean'))
 }
 
-function packToken(net) {
+async function packToken(net) {
   console.time(greenFont(`build-${net}-tokens`))
 
   const folder = path.join(__dirname, `./tokens/${NET_FOLDERS[net]}`)
-  const infos = getTokensInfo(folder)
+  const infos = await getTokensInfo(folder)
   let result = []
-  const listJson = infos.map(item => {
-    return {
-      ...item,
-      imgName: rename(item.img) + '.png'
-    }
-  })
+  const listJson = infos
+    .sort((a, b) => {
+      if (a.createTime < b.createTime) {
+        return -1
+      } else {
+        return 1
+      }
+    })
+    .map(item => {
+      return {
+        ...item,
+        imgName: rename(item.img) + '.png'
+      }
+    })
 
   file.mkdirSync(ASSETS)
 
@@ -66,30 +74,36 @@ function packToken(net) {
     })
   })
 
-  console.table(result, ['name', 'symbol', 'decimals', 'address'])
-  
+  console.table(listJson, [
+    'name',
+    'symbol',
+    'decimals',
+    'address',
+    'createTime'
+  ])
+
   file.writeFileSync(
     path.join(__dirname, `./dist/${net}.json`),
     JSON.stringify(result, null, 2)
   )
   console.timeEnd(greenFont(`build-${net}-tokens`))
-
 }
 
 function rename(img) {
   return hashName.sync(img)
 }
 
-function getTokensInfo(folder) {
+async function getTokensInfo(folder) {
   const tokens = getTokens(folder)
   const result = []
-  tokens
-    .filter(item => {
-      return !item.startsWith('.')
-    })
-    .forEach(item => {
-      result.push(tokenInfo(path.join(folder, item)))
-    })
+  const temp = tokens.filter(item => {
+    return !item.startsWith('.')
+  })
+
+  for (let i = 0; i < temp.length; i++) {
+    const item = temp[i]
+    result.push(await tokenInfo(path.join(folder, item)))
+  }
 
   return result
 }
@@ -98,12 +112,29 @@ function getTokens(folder) {
   return file.readdirSync(folder)
 }
 
-function tokenInfo(tokenPath) {
+async function tokenInfo(tokenPath) {
   const file = path.join(tokenPath, 'info.json')
   const img = path.join(tokenPath, 'token.png')
   const info = require(file)
   info.img = img
+  info.createTime = await getCreateTimeFromGit(tokenPath)
   return info
+}
+
+async function getCreateTimeFromGit(dirPath) {
+  const command =
+    'git log --diff-filter=A --follow --format=%aD -1 -- [path] | head -1'
+  return new Promise((resolve, reject) => {
+    exec(command.replace('[path]', dirPath), (err, stdout, stderr) => {
+      if (err) return reject(err)
+      if (stderr) return reject(stderr)
+      if (!stdout)
+        return reject(
+          new Error('Can not find create time from git for dir: ' + dirPath)
+        )
+      return resolve(new Date(stdout))
+    })
+  })
 }
 
 module.exports = {
