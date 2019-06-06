d026948dcf7b0e5d95e5be65fe55be75c3841a10
Asbert Ma <stayinalive@msn.cn>
2019-06-06 16:37:17 +0800

    index async

--- index.js
+++ index.js
@@ -1,20 +1,27 @@
 const { clean, build } = require('./script')
 const action = process.argv[2]
-
-switch (action) {
-  case 'main':
-    build('main')
-    break
-
-  case 'test':
-    build('test')
-    break
-  case 'clean':
-    clean()
-    break
-  default:
-    clean()
-    build('main')
-    build('test')
-    break
+async function buildAll() {
+  await clean()
+  await build('main')
+  await build('test')
 }
+
+async function start() {
+  switch (action) {
+    case 'main':
+      await build('main')
+      break
+
+    case 'test':
+      await build('test')
+      break
+    case 'clean':
+      await clean()
+      break
+    default:
+      await buildAll()
+      break
+  }
+}
+
+start()
